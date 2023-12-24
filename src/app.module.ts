4c039f879e63db30ca9bc12ec8d3eb37b8413254
Naelson <naelso.nuness@gmail.com>
2023-12-24 20:50:25 -0300

    chore: 'add swagger'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { databaseProviders } from './config/database.providers';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ...databaseProviders],
 })
 export class AppModule {}
