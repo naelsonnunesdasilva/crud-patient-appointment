f81859f861bd4fea5fd09e0b84d196d1968496dc
Naelson <naelso.nuness@gmail.com>
2023-12-29 17:48:32 -0300

    chore: add env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PatientModule } from './modules/patient/patient.module';
 import { AppointmentModule } from './modules/appointment/appointment.module';
-import { PatientEntity } from 'src/database/patient.entity';
-import { AppointmentEntity } from 'src/database/appointment.entity';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './modules/auth/auth.guard';
 import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmConfigService } from 'src/config/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      entities: [PatientEntity, AppointmentEntity],
-      synchronize: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
     PatientModule,
     AppointmentModule,
     AuthModule
---
+++ src/migrations/1703875751405-data-patient.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class DataPatient1703875751405 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`INSERT INTO 
+        Patient(id, name, email, cpf) 
+        VALUES(1, 'lorem', 'lorem@gmail.com', '111.111.111-11')`);
+
+        await queryRunner.query(`INSERT INTO 
+        Patient(id, name, email, cpf) 
+        VALUES(2, 'maria', 'maria@gmail.com', '222.222.222-22')`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DELETE FROM 
+        Patient 
+        WHERE id=1`);
+
+        await queryRunner.query(`DELETE FROM 
+        Patient 
+        WHERE id=2`);
+    }
+
+}
