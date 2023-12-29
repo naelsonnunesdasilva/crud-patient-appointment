ec6d669fe8d164d5241829f291c353d29ba46573
Naelson <naelso.nuness@gmail.com>
2023-12-29 19:05:52 -0300

    chore: remove duplicate config

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,22 +1,29 @@
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
-import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+
+const config = {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'test',
+    entities: ['dist/**/*.entity.js'],
+    logging: true,
+    synchronize: false,
+    migrationsRun: false,
+    migrations: ['dist/migrations/*.js'],
+    migrationsTableName: 'migrations',
+} as any;
+
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
-    return {
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      logging: !!JSON.parse(
-        process.env.DB_LOGGING || 'false',
-      ),
-      autoLoadEntities: true,
-    };
+    return config;
   }
 }
+
+export const AppDataSource = new DataSource(config);
